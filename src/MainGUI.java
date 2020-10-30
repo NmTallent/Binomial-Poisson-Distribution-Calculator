83b953249b158a84f7d8389003647f717ca96536
NmTallent <nicktallent98@gmail.com>
2020-10-30 12:13:05 -0400

    Added .gitignore file

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -15,6 +15,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
